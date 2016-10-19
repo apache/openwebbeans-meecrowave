f77b045e21c135e323e6ca722b4464c5d7a09a67
Romain Manni-Bucau <rmannibucau@apache.org>
2016-10-19 08:13:59 +0000

    simple test with cdi activated
    git-svn-id: https://svn.apache.org/repos/asf/openwebbeans/microwave/trunk@1765551 13f79535-47bb-0310-9956-ffa450edef68

--- microwave-core/src/test/java/org/superbiz/app/Injectable.java
+++ microwave-core/src/test/java/org/superbiz/app/Injectable.java
@@ -18,40 +18,11 @@
  */
 package org.superbiz.app;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import javax.enterprise.context.ApplicationScoped;
-import javax.inject.Inject;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 
-@Path("test")
 @ApplicationScoped
-public class Endpoint {
-    @Inject
-    private Injectable injectable;
-
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String simple() {
-        return Boolean.parseBoolean(injectable.injected()) ? "simple" : "fail";
-    }
-
-    @GET
-    @Path("json")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Simple json() {
-        return new Simple("test");
-    }
-
-    @Data
-    @AllArgsConstructor
-    @NoArgsConstructor
-    public static class Simple {
-        private String name;
+public class Injectable {
+    public String injected() {
+        return "true";
     }
 }
