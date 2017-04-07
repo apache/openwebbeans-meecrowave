11dab86d418cf835760a242ecc36df1aadee8297
Romain Manni-Bucau <rmannibucau@apache.org>
2017-04-07 07:22:07 +0000

    MEECROWAVE-23 sample project and doc fix
    git-svn-id: https://svn.apache.org/repos/asf/openwebbeans/meecrowave/trunk@1790512 13f79535-47bb-0310-9956-ffa450edef68

---
+++ sample/src/test/java/com/superbiz/jaxrs/HelloEndpointTest.java
@@ -0,0 +1,52 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.superbiz.jaxrs;
+
+import org.apache.meecrowave.Meecrowave;
+import org.apache.meecrowave.junit.MonoMeecrowave;
+import org.apache.meecrowave.testing.ConfigurationInject;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON_TYPE;
+import static org.junit.Assert.assertEquals;
+
+@RunWith(MonoMeecrowave.Runner.class)
+public class HelloEndpointTest {
+    @ConfigurationInject
+    private Meecrowave.Builder configuration;
+
+    @Test
+    public void hello() {
+        final Client client = ClientBuilder.newClient();
+        try {
+            assertEquals("me", client.target("http://localhost:" + configuration.getHttpPort())
+                    .path("api/hello")
+                    .queryParam("name", "me")
+                    .request(APPLICATION_JSON_TYPE)
+                    .get(HelloEndpoint.Hello.class)
+                    .getName());
+        } finally {
+            client.close();
+        }
+    }
+}
