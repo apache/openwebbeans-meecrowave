cbdaabe767cf1300df0d25a84d6d601a95b40e1f
Romain Manni-Bucau <rmannibucau@apache.org>
2017-04-12 16:45:29 +0000

    some check to ensure we don't redefine what is already there when adding jsp servlet
    git-svn-id: https://svn.apache.org/repos/asf/openwebbeans/meecrowave/trunk@1791148 13f79535-47bb-0310-9956-ffa450edef68

--- meecrowave-core/src/main/java/org/apache/meecrowave/tomcat/TomcatAutoInitializer.java
+++ meecrowave-core/src/main/java/org/apache/meecrowave/tomcat/TomcatAutoInitializer.java
@@ -44,13 +44,15 @@ public class TomcatAutoInitializer implements ServletContainerInitializer {
         try {
             final String jsp = "org.apache.jasper.servlet.JspServlet";
             TomcatAutoInitializer.class.getClassLoader().loadClass(jsp);
-            final ServletRegistration.Dynamic jspDef = ctx.addServlet("default", jsp);
-            jspDef.setInitParameter("fork", "false");
-            jspDef.setInitParameter("xpoweredBy", "false");
-            jspDef.setInitParameter("development", "false");
-            jspDef.setLoadOnStartup(3);
-            def.addMapping("*.jsp");
-            def.addMapping("*.jspx");
+            final ServletRegistration.Dynamic jspDef = ctx.addServlet("jsp", jsp);
+            if (jspDef != null) {
+                jspDef.setInitParameter("fork", "false");
+                jspDef.setInitParameter("xpoweredBy", "false");
+                jspDef.setInitParameter("development", "false");
+                jspDef.setLoadOnStartup(3);
+                def.addMapping("*.jsp");
+                def.addMapping("*.jspx");
+            }
         } catch (final NoClassDefFoundError | ClassNotFoundException e) {
             // not there, skip
         }
