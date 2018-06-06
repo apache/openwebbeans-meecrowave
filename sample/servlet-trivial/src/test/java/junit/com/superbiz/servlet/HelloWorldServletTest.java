f5966378f02aafd5b09d4a61764fc7b699e7cebe
Sven Ruppert <sven.ruppert@gmail.com>
2018-06-06 09:47:41 +0200

    removed dep. okhttp

--- sample/servlet-trivial/src/test/java/junit/com/superbiz/servlet/HelloWorldServletTest.java
+++ sample/servlet-trivial/src/test/java/junit/com/superbiz/servlet/HelloWorldServletTest.java
@@ -18,15 +18,16 @@
  */
 package junit.com.superbiz.servlet;
 
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
 import org.apache.meecrowave.junit.MeecrowaveRule;
 import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 
-import java.io.IOException;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON_TYPE;
+import static org.junit.Assert.assertEquals;
 
 
 public class HelloWorldServletTest {
@@ -35,17 +36,18 @@ public class HelloWorldServletTest {
   public final MeecrowaveRule rule = new MeecrowaveRule();
 
   @Test
-  public void test001() throws IOException {
-
-    OkHttpClient client = new OkHttpClient();
-
-    Request request = new Request.Builder()
-        .url("http://127.0.0.1:" + rule.getConfiguration().getHttpPort() + "/?value=HalloNase")
-        .get()
-        .build();
-    Response response = client.newCall(request).execute();
-    String   string   = response.body().string().trim();
-    System.out.println("string = " + string);
-    Assert.assertEquals("HALLONASE", string);
+  public void test001() {
+    final Client client = ClientBuilder.newClient();
+    try {
+      assertEquals("HALLONASE", client.target("http://127.0.0.1:" + rule.getConfiguration().getHttpPort())
+                                      .queryParam("value", "HalloNase")
+                                      .request(APPLICATION_JSON_TYPE)
+                                      .get(String.class)
+                                      .trim());
+    } catch (Exception e) {
+      Assert.fail(e.getMessage());
+    } finally {
+      client.close();
+    }
   }
 }
